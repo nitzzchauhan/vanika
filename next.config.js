0311429ade34eebb548daea33c5d31be1480e57a
Team Tathastu <nitinkiranchauhan@gmail.com>
2026-08-26 00:41:03 +0530

    fix: move typedRoutes to top-level for Next.js 16

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typedRoutes: true,
   images: {
     remotePatterns: [
       {
@@ -11,9 +12,6 @@ const nextConfig = {
         hostname: "images.unsplash.com",
       },
     ],
-  },
-  experimental: {
-    typedRoutes: true,
   },
 };
 
